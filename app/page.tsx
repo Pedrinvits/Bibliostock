271bf85929cccead663a27b6ce0dc081cc810d36
pedro <pedromantovani26@gmail.com>
2024-09-22 14:13:01 -0300

    feat : add suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import LibraryInventorySystem from "@/components/library-inventory";
+import Loading from "@/components/loader";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <main>
       <ModeToggle/>
-      <LibraryInventorySystem />
+      <Suspense fallback={<Loading />}>
+        <LibraryInventorySystem />
+      </Suspense>
     </main>
   );
 }
---
+++ components/loader/index.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    return (
+        <div className="container mx-auto p-4 bg-card shadow-2xl rounded-lg m-2 w-[90%] sm:w-[90%]">
+            <h1>Loading ...</h1>
+        </div>
+    );
+}
+  
